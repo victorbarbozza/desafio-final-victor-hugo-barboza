f0cfa150b9c50c5fdff68756d0da35881470b0f8
Victor <victorhugobarboza@hotmail.com>
2025-02-06 20:39:09 -0300

    feat: datasource

---
+++ src/config/database.ts
@@ -0,0 +1,20 @@
+import { DataSource, DataSourceOptions } from 'typeorm'
+
+const databaseConfig: DataSourceOptions = {
+  type: 'sqlite',
+  database: './src/repository/dastabase.sqlite',
+  entities: ['./src/models/*.ts'],
+  synchronize: true, //usar somente em desenvolvimento (sinconiza as tabelas e pode perder dados)
+}
+
+export const AppDataSource = new DataSource(databaseConfig)
+
+export const initializeDatabase = async () => {
+  try {
+    await AppDataSource.initialize()
+    console.log('Banco de dados conectado com sucesso.')
+  } catch (error) {
+    console.error('Erro ao conectar no banco:', error)
+    // process.exit(1)
+  }
+}
