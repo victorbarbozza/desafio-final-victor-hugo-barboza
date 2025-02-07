6d337f83c3720de18ed6f0e670b8e44abd1a33c5
Victor <victorhugobarboza@hotmail.com>
2025-02-07 19:06:27 -0300

    chore: rename variable

--- src/routes/clientRoutes.ts
+++ src/routes/clientRoutes.ts
@@ -1,42 +1,42 @@
 import { Router } from 'express'
 import { ClientController } from '~/controllers/clientController'
 
-const clientsRoutes = Router()
+const clientsRouter = Router()
 const clientController = new ClientController()
 
 // Rota para criar um novo cliente
-clientsRoutes.post('/clients', (req, res) => {
+clientsRouter.post('/clients', (req, res) => {
   clientController.create(req, res)
 })
 
 // Rota para contar clientes
-clientsRoutes.get('/clients/count', (req, res) => {
+clientsRouter.get('/clients/count', (req, res) => {
   clientController.count(req, res)
 })
 
 // Rota para buscar todos os clientes
-clientsRoutes.get('/clients', (req, res) => {
+clientsRouter.get('/clients', (req, res) => {
   clientController.findAll(req, res)
 })
 
 // Rota para buscar um cliente pelo nome
-clientsRoutes.get('/clients/name/:name', (req, res) => {
+clientsRouter.get('/clients/name/:name', (req, res) => {
   clientController.findByName(req, res)
 })
 
 // Rota para buscar um cliente pelo ID
-clientsRoutes.get('/clients/:id', (req, res) => {
+clientsRouter.get('/clients/:id', (req, res) => {
   clientController.findById(req, res)
 })
 
 // Rota para atualizar um cliente pelo ID
-clientsRoutes.put('/clients/:id', (req, res) => {
+clientsRouter.put('/clients/:id', (req, res) => {
   clientController.update(req, res)
 })
 
 // Rota para deletar um cliente pelo ID
-clientsRoutes.delete('/clients/:id', (req, res) => {
+clientsRouter.delete('/clients/:id', (req, res) => {
   clientController.delete(req, res)
 })
 
-export { clientsRoutes }
+export { clientsRouter }
