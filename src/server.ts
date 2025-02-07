0196deb96a4156b47cc029cdd6fd6132c5093f90
Victor <victorhugobarboza@hotmail.com>
2025-02-07 19:01:38 -0300

    feat: cors and unique file

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,17 @@
-import { app } from './app'
+import express from 'express'
+import cors from 'cors'
+
+import { clientsRouter } from '~/routes/clientRoutes'
+import { adminRouter } from './routes/adminRoute'
 import { initializeDatabase } from './config/database'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api', clientsRouter)
+app.use('/', adminRouter)
 
 const port = 3000
 
